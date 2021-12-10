efed081dad768ea26a57c45365913eef01a9c430
Stanislav Sidoryak <stanislav.sidoryak@cma.ru>
2021-12-10 14:10:23 +0700

    tasks 1607 added

--- 2.JavaCore/src/com/javarush/task/task16/task1607/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1607/Solution.java
@@ -18,7 +18,15 @@ public class Solution {
     public static int calculateHorsesFinished(List<Horse> horses) throws InterruptedException {
         int finishedCount = 0;
         //напишите тут ваш код
+        for (Horse horse: horses) {
+            if (horse.isFinished()){
+                finishedCount++;
+            } else {
+                System.out.println("Waiting for " + horse.getName());
+                horse.join();
 
+            }
+        }
         return finishedCount;
     }
 
