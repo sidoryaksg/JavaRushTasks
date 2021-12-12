86aae78a845f7d20288c3d65dd8062a47e24de36
Stanislav Sidoryak <stanislav.sidoryak@cma.ru>
2021-12-12 14:50:40 +0700

    tasks 1617 added

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1617/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task16.task1617;
+
+/* 
+Отсчет на гонках
+*/
+
+public class Solution {
+    public static volatile int numSeconds = 3;
+
+    public static void main(String[] args) throws InterruptedException {
+        RacingClock clock = new RacingClock();
+        //add your code here - добавь код тут
+        Thread.sleep(3500);
+        clock.interrupt();
+    }
+
+    public static class RacingClock extends Thread {
+        public RacingClock() {
+            start();
+        }
+
+        public void run() {
+            //add your code here - добавь код тут
+            try {
+                while (interrupted() != true && numSeconds >= 0) {
+                    if (numSeconds == 0) {
+                        System.out.println("Марш!");
+                    } else {
+                        System.out.print(numSeconds + " ");
+                        Thread.sleep(1000);
+                    }
+                    numSeconds--;
+                }
+            } catch(InterruptedException e){
+                if (numSeconds != -1) {
+                    System.out.println("Прервано!");
+                }
+
+                    }
+
+                }
+
+
+            }
+
+        }
