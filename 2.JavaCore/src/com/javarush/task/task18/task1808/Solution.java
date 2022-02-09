a8020223a7c5b4cfe9624b8f503d830dd54d0be9
wilddancer <sidoryak@gmail.com>
2022-02-09 14:53:46 +0700

    task 1808 added

--- 2.JavaCore/src/com/javarush/task/task18/task1808/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1808/Solution.java
@@ -5,9 +5,28 @@ import java.io.*;
 /* 
 Разделение файла
 */
+//C:\temp\1.txt
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String file1 = bufferedReader.readLine();
+        String file2 = bufferedReader.readLine();
+        String file3 = bufferedReader.readLine();
+
+        FileInputStream fileInputStream1 = new FileInputStream(file1);
+        FileOutputStream fileOutputStream1 = new FileOutputStream(file2);
+        FileOutputStream fileOutputStream2 = new FileOutputStream(file3);
+        int fileLength = fileInputStream1.available();
+            while ( (fileLength/2) < fileInputStream1.available()) {
+                fileOutputStream1.write(fileInputStream1.read());
+        }
+            fileOutputStream1.close();
+            while (fileInputStream1.available() > 0){
+                fileOutputStream2.write(fileInputStream1.read());
+            }
+            fileOutputStream2.close();
+            fileInputStream1.close();
 
     }
 }
