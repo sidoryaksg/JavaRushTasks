257b214b2d9a70e677198618d2b2c66dba701f3f
wilddancer <sidoryak@gmail.com>
2022-02-09 14:35:58 +0700

    task 1807 added

--- 2.JavaCore/src/com/javarush/task/task18/task1807/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1807/Solution.java
@@ -2,20 +2,29 @@ package com.javarush.task.task18.task1807;
 
 import java.io.*;
 
+
+
+
 /* 
 Подсчет запятых
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         String filename = "";
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         filename = bufferedReader.readLine();
-        }
-        catch (IOException e){}
-        System.out.println(filename);
 
-        try (InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(filename))){
+        int count = 0;
+
+        try (FileInputStream fileInputStream = new FileInputStream(filename)){
+            while (fileInputStream.available() > 0){
+
+                    if (fileInputStream.read() == 44) count++;
+
+            }
+
+
 
 
         }
@@ -26,6 +35,7 @@ public class Solution {
         catch (IOException e){
             System.out.println("IO e");
         }
+        System.out.println(count);
 
 
     }
