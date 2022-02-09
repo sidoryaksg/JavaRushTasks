f74dfd91f98c87e44d3f0d5dff4a2cc2dcfd345b
Stanislav Sidoryak <stanislav.sidoryak@cma.ru>
2022-02-09 23:17:53 +0700

    tasks 1807 added

--- 2.JavaCore/src/com/javarush/task/task18/task1807/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1807/Solution.java
@@ -1,42 +1,15 @@
 package com.javarush.task.task18.task1807;
 
-import java.io.*;
-
-
-
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /* 
 Подсчет запятых
 */
 
 public class Solution {
-    public static void main(String[] args) throws IOException{
-        String filename = "";
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        filename = bufferedReader.readLine();
-
-        int count = 0;
-
-        try (FileInputStream fileInputStream = new FileInputStream(filename)){
-            while (fileInputStream.available() > 0){
-
-                    if (fileInputStream.read() == 44) count++;
-
-            }
-
-
-
-
-        }
-        catch (FileNotFoundException e) {
-            System.out.println("FNF e");
-
-        }
-        catch (IOException e){
-            System.out.println("IO e");
-        }
-        System.out.println(count);
-
-
+    public static void main(String[] args) {
     }
 }
