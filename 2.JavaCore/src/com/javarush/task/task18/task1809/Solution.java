44454b024499fc2dedb2839b117fe7c65261361f
wilddancer <sidoryak@gmail.com>
2022-02-09 15:11:45 +0700

    task 1809 added

--- 2.JavaCore/src/com/javarush/task/task18/task1809/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1809/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task18.task1809;
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -10,7 +11,22 @@ import java.util.List;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String file1 = bufferedReader.readLine();
+        String file2 = bufferedReader.readLine();
+        FileInputStream fis = new FileInputStream(file1);
+        FileOutputStream fos = new FileOutputStream(file2);
+        List<Integer> inputBytes = new ArrayList<>();
+        while (fis.available()>0 ) inputBytes.add(fis.read());
+        fis.close();
+        Collections.reverse(inputBytes);
+        for (int i = 0; i < inputBytes.size(); i++) {
+            fos.write(inputBytes.get(i));
+
+        }
+        fos.close();
+
 
     }
 }
