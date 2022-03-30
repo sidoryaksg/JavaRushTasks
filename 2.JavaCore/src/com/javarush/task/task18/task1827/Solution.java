6c69a9dd2f9f6ccf670831bcf9422936d4344320
wilddancer <sidoryak@gmail.com>
2022-03-30 18:50:30 +0700

    part of 1827

--- 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -12,6 +12,34 @@ import java.util.List;
 */
 
 public class Solution {
+    public static class Product {
+        int id;
+        String productName;
+        String price;
+        String quantity;
+
+        public Product (int id, String productName, String price, String quantity){
+            this.id = id;
+            this.productName = productName;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+    }
+
+
     public static void main(String[] args) throws Exception {
+        if (args.length == 0 )
+            return;
+
+
+
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(System.in));
+        String path = bufferedReader.readLine();
+        List<Product> products = new ArrayList<>();
+
+        if (args[0].equals("-c")) {
+
+        }
     }
 }
