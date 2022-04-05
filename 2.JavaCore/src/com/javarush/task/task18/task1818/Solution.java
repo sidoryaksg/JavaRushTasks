4fdcd675149ecd9013d2a990b357cd920a3f40c6
wilddancer <sidoryak@gmail.com>
2022-04-05 12:46:38 +0700

    1818 done

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1818/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task18.task1818;
+
+import java.io.*;
+
+/* 
+Два в одном
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String file1 = reader.readLine();
+        String file2 = reader.readLine();
+        String file3 = reader.readLine();
+
+        try (FileWriter file1Writer = new FileWriter(file1);
+             FileReader file2Reader = new FileReader(file2);
+             FileReader file3Reader = new FileReader(file3)
+                ) {
+            while (file2Reader.ready()){
+                file1Writer.write(file2Reader.read());
+            }
+            while (file3Reader.ready()){
+                file1Writer.write(file3Reader.read());
+            }
+        }
+    }
+}
