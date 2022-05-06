4fe4cba049c29e49c21c9ad6ce48a3f133b45a2e
Stanislav Sidoryak <stanislav.sidoryak@cma.ru>
2022-05-06 11:16:21 +0700

    tasks 1823 completed

--- 2.JavaCore/src/com/javarush/task/task18/task1823/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1823/Solution.java
@@ -1,9 +1,6 @@
 package com.javarush.task.task18.task1823;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,23 +11,46 @@ import java.util.Map;
 public class Solution {
     public static Map<String, Integer> resultMap = new HashMap<String, Integer>();
 
-    public static void main(String[] args)  throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        while (true){
-            String filename = bufferedReader.readLine();
-            if (filename.equals("exit")){
-                return;
-            }
-            ReadThread readThread = new ReadThread(filename);
-            readThread.start();
+        String fileName;
+        while (!((fileName = bufferedReader.readLine()).equals("exit"))) {
+            new ReadThread(fileName).start();
         }
 
     }
 
     public static class ReadThread extends Thread {
+        private String fileName;
+
         public ReadThread(String fileName) {
+            this.fileName = fileName;
             //implement constructor body
         }
+
         // implement file reading here - реализуйте чтение из файла тут
+        @Override
+        public void run() {
+            byte[] byteCounts = new byte[256];
+            try (FileInputStream fileInputStream = new FileInputStream(fileName)) {
+                while (fileInputStream.available() > 0) {
+                    int bytesRead = fileInputStream.read();
+                    byteCounts[bytesRead]++;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            int maxCount = 0;
+            int maxCountByte = 0;
+
+            for (int i = 0; i < byteCounts.length; i++) {
+                if (byteCounts[i] > maxCount) {
+                    maxCount = byteCounts[i];
+                    maxCountByte = i;
+                }
+            }
+            resultMap.put(fileName, maxCountByte);
+        }
+
     }
 }
